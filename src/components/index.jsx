d0dc6bee7751071ebf4bf9c5ea4e8bb07878ff4e
Syedmoiz7 <syedzada7866@gmail.com>
2022-10-18 12:13:33 +0500

    dark mode added

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,20 +2,26 @@ import { useState } from "react";
 import './index.css'
 
 const Counter = () => {
-    const [counting, setCounting] = useState(1)
+    const [counting, setCounting] = useState(0);
+    const [isLit, setLit] = useState([true]);
 
-
+    const clickHandler = () => {
+        setLit(!isLit)
+    }
     return (
-        <div className="mainContainer">
+        <div className={`mainContainer ${(isLit) ? "Lit" : "Dark"}`}>
             <div className="heading">
                 <h1>Counter App</h1>
             </div>
             <div className="counter">
                 {counting}
-                </div>
-                <button onClick={() => {setCounting(counting + 1 )}}>Addition</button>
-                <button onClick={() => {setCounting(counting + -1 )}}>Subtraction</button>
-
+            </div>
+            <button onClick={() => { setCounting(counting + 1) }} className="bt">Addition</button>
+            <button onClick={() => { setCounting(counting + -1) }} className="bt">Subtraction</button> <br />
+            <button onClick={() => { setCounting(0) }} className="bt">Reset</button>
+            <div className="mode">
+                <button onClick={clickHandler}></button>
+            </div>
         </div>
     )
 }
